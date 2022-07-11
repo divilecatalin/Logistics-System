f17233f76295e36ea2a2961bacb188295ac3a586
stefanviorel.popescu@gmail.com <stefanviorel.popescu@gmail.com>
2022-07-11 09:58:37 +0300

    Initial commit

---
+++ src/main/java/com/java/logisticssystem/LogisticsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.java.logisticssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsSystemApplication.class, args);
+	}
+
+}
